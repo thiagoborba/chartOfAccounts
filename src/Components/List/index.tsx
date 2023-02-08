4ea5df4195ec0406a8d313c962fb6faba54e2667
Thiago <thborba1@gmail.com>
2023-02-07 22:09:49 -0300

    feat: improves flatList

--- src/Components/List/index.tsx
+++ src/Components/List/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import {
   FlatList,
   HStack,
@@ -10,7 +10,12 @@ import {
 import { Feather } from "@expo/vector-icons";
 import { AccountTypes } from "../../Types";
 import { IFlatListProps } from "native-base/lib/typescript/components/basic/FlatList";
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ListRenderItem,
+} from "react-native";
 import { theme } from "../../theme";
 
 type ItemProps = {
@@ -22,46 +27,6 @@ type ItemProps = {
 };
 
 type ListProps = Partial<IFlatListProps<ItemProps>>;
-
-export const ItemList: React.FC<ItemProps> = ({
-  label,
-  onClickDelete,
-  onClickItem,
-  id,
-  type,
-}) => {
-  const color =
-    type === AccountTypes.EXPENSE
-      ? theme.colors.UCondo.danger
-      : theme.colors.UCondo.sucess;
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: theme.colors.white,
-      borderRadius: theme.radii["2xl"],
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: theme.space[5],
-    },
-    title: {
-      fontFamily: theme.fonts.UCondo.body,
-      color: color,
-    },
-  });
-
-  return (
-    <TouchableOpacity style={styles.container} onPress={onClickItem}>
-      <Text style={styles.title}>
-        {id} - {label}
-      </Text>
-      <TouchableOpacity onPress={onClickDelete}>
-        <Icon as={Feather} color="UCondo.grey" name="trash" />
-      </TouchableOpacity>
-    </TouchableOpacity>
-  );
-};
 
 export const List = (props: ListProps) => {
   function ListHeader() {
@@ -80,17 +45,61 @@ export const List = (props: ListProps) => {
     );
   }
 
+  const ItemList: ListRenderItem<ItemProps> = ({
+    item: { id, type, label, onClickDelete, onClickItem },
+  }) => {
+    const color =
+      type === AccountTypes.EXPENSE
+        ? theme.colors.UCondo.danger
+        : theme.colors.UCondo.sucess;
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: theme.colors.white,
+        borderRadius: theme.radii["2xl"],
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingLeft: theme.space[4],
+      },
+      title: {
+        fontFamily: theme.fonts.UCondo.body,
+        color: color,
+      },
+      button: {
+        margin: 0,
+        justifyContent: "center",
+        alignItems: "center",
+        height: theme.sizes["16"],
+        width: theme.sizes["16"],
+        borderRadius: theme.radii["full"],
+      },
+    });
+
+    return (
+      <TouchableOpacity key={id} style={styles.container} onPress={onClickItem}>
+        <Text style={styles.title}>
+          {id} - {label}
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={onClickDelete}>
+          <Icon as={Feather} color="UCondo.grey" name="trash" />
+        </TouchableOpacity>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <FlatList
       ListHeaderComponent={<ListHeader />}
       stickyHeaderIndices={[0]}
       ItemSeparatorComponent={() => <HStack key={null} height={3} />}
       data={props.data}
-      renderItem={({ item }) => <ItemList {...item} />}
+      renderItem={ItemList}
       keyExtractor={(item) => item.id}
-      removeClippedSubviews={true}
-      maxToRenderPerBatch={20}
-      initialNumToRender={20}
+      // removeClippedSubviews={true}
+      // maxToRenderPerBatch={20}
+      // initialNumToRender={20}
       {...props}
     />
   );
